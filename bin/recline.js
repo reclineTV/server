4172c60a0e164f2b17b9e84a605e7ab8199d4dfd
Luke Briggs <luke@kulestar.com>
2018-01-20 18:35:36 +0000

    Added logging to service startup

--- bin/recline.js
+++ bin/recline.js
@@ -3,6 +3,7 @@
 var service = require ("os-service");
 var process = require('process');
 var path = require('path');
+var fs = require('fs');
 
 // The working dir where recline was invoked:
 var calledFromPath = process.cwd();
@@ -10,16 +11,31 @@ var calledFromPath = process.cwd();
 // Change to the Recline dir:
 process.chdir(path.dirname(__filename) + '/../');
 
-require('../source/start.js')({
-	loadCommandLine: true,
-	calledFromPath
-});
-
-// Also make sure auto start is setup:
+// Make sure auto start is setup:
 service.add ("recline", {
 	programPath: "recline"
 }, function(error){
 	if(error){
 		console.log('Unable to add Recline as a service', error);
 	}
+});
+
+// Start the service now:
+var logStream = fs.createWriteStream ("recline.log");
+
+var _app;
+
+require('../source/start.js')({
+	loadCommandLine: true,
+	calledFromPath
+}).then(app => _app = app);
+
+// Use the run line to redirect output to a file and listen for stop requests:
+service.run (logStream, function () {
+	
+	if(_app){
+		// _app.express.stop();
+	}
+	
+	service.stop();
 });
