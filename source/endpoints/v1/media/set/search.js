90f060a8ba6660b42c78cb23960c10ea5918216f
Luke Briggs <luke@kulestar.com>
2018-02-04 21:19:18 +0000

    Search media sets via their parent

--- source/endpoints/v1/media/set/search.js
+++ source/endpoints/v1/media/set/search.js
@@ -1,17 +1,20 @@
 module.exports = app => (request, response) => {
 	// Search media sets.
 	
+	/*
 	if(!request.user){
 		return response.error('action/notAuthorized');
 	}
+	*/
 	
 	let {
-		query
+		query,
+		parent
 	} = request.body;
 	
 	query=(query || '') + '%';
 	
-	app.database.query('select * from media_sets where `title` like ? and deleted=0', [query], (err, results) => {
+	app.database.query('select * from media_sets where `title` like ? and deleted=0 and parentSet ' + (parent ? '=?' : 'is null'), [query, parent], (err, results) => {
 		if(err){
 			return response.error('media/error', err);
 		}
