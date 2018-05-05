9dc5e48987d153d119abfcf3342529abbc305e40
Luke Briggs <luke@kulestar.com>
2018-05-05 23:59:40 +0100

    Added ability to resize all images in a dir

--- source/modules/resize/index.js
+++ source/modules/resize/index.js
@@ -1,8 +1,56 @@
 const fs = require('fs');
 const resizeImg = require('resize-img');
+var recursive = require("recursive-readdir");
+var async = require("async");
 
 
 module.exports = app => {
+	
+	/*
+	* Destructively resizes all images in the given directory.
+	* -poster.* uses poster settings, and -backdrop.* uses backdrop settings.
+	*/
+	app.resizeAll = directory => {
+		
+		return new Promise((success, reject) => {
+			recursive(directory, function (err, files) {
+				if(err){
+					return reject(err);
+				}
+				
+				// Resize in series:
+				async.mapSeries(files, (file, cb) => {
+					
+					// Always save as a jpg:
+					var pathParts = file.split('.');
+					pathParts[pathParts.length-1] = 'jpg';
+					var target = pathParts.join('.');
+					
+					if(file.indexOf('poster.') != -1){
+						// Got a poster - resize it:
+						app.resizePoster(file, target).then(cb).catch(reject);
+					}else if(file.indexOf('backdrop.') != -1){
+						// backdrop - resize it:
+						app.resizeBackdrop(file, target).then(cb).catch(reject);
+					}else{
+						return cb();
+					}
+					
+				}, success);
+				
+			});
+		});
+	},
+	
+	/*
+	* Resizes an image using backdrop defaults
+	*/
+	app.resizeBackdrop = (fromFile, toFile) => app.resizeImage(fromFile, toFile, {width: 1920}),
+	
+	/*
+	* Resizes an image using poster defaults
+	*/
+	app.resizePoster = (fromFile, toFile) => app.resizeImage(fromFile, toFile, {width: 250}),
 	
 	/*
 	Resizes an image from the given original to the given target size (specified as width and height in options)
